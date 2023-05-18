a9bc8f2bd775fea2572a9ff712a59abc88c91882
Alejandra <alevy@securityscorecard.io>
2023-05-18 14:25:37 -0300

    Add markdown support on chat

--- norman-fe/src/components/MessageBox.tsx
+++ norman-fe/src/components/MessageBox.tsx
@@ -1,10 +1,40 @@
+import { CSSProperties } from "react";
+import ReactMarkdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+// import { dracula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { styled } from "styled-components";
 import Message from "../types/Message";
+import dracula from "../styles/dracula";
 
 const MessageBox: React.FC<MessageBoxProps> = ({ message }) => {
+  const codeStyles: { [key: string]: CSSProperties } = {
+    ...dracula,
+  };
   return (
     <MessageContainer key={message.id} message={message}>
-      {message.text}
+      <ReactMarkdown
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={codeStyles as any}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {message.text}
+      </ReactMarkdown>
     </MessageContainer>
   );
 };
